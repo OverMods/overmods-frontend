82e5641fba603dd5060927c6ac7f7263f3b3abd4
mykola2312 <49044616+mykola2312@users.noreply.github.com>
2023-06-25 00:30:35 +0300

    Implement comments store for vuex

--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,25 @@
 import { createStore } from "vuex";
 import { HTTP, getUploadUrl } from "../http.js";
-import { renderMarkdown } from "../utils.js";
+import {relativeDate, renderMarkdown} from "../utils.js";
 
 export default createStore({
     state: {
         gameList: [],
         game: null,
-        mods: null
+        mods: null,
+        mod: null,
+        author: null,
+        screenshots: null,
+        comments: null
     },
     getters: {
         getGameList: (store) => store.gameList,
         getGame: (store) => store.game,
-        getMods: (store) => store.mods
+        getMods: (store) => store.mods,
+        getMod: (store) => store.mod,
+        getAuthor: (store) => store.author,
+        getScreenshots: (store) => store.screenshots,
+        getComments: (store) => store.comments
     },
     actions: {
         async fetchGameList({ commit }) {
@@ -32,6 +40,39 @@ export default createStore({
 
                 commit("SET_GAME", res.data.game);
                 commit("SET_MODS", res.data.mods);
+            } catch (e) {
+                console.log(e);
+            }
+        },
+        async fetchMod({ commit }, id) {
+            try {
+                const mod = await HTTP.get(`/mod/${id}`);
+                if (mod.data.error) {
+                    console.log(mod.data.error);
+                    return;
+                }
+                commit("SET_MOD", mod.data);
+
+                const author = await HTTP.get(`/user/${mod.data.author}`);
+                if (author.data.error) {
+                    console.log(author.data.error);
+                    return;
+                }
+                commit("SET_AUTHOR", author.data);
+
+                const screenshots = await HTTP.get(`/mod/${id}/screenshot`);
+                if (screenshots.data.error) {
+                    console.log(screenshots.data.error);
+                    return;
+                }
+                commit("SET_SCREENSHOTS", screenshots.data);
+
+                const comments = await HTTP.get(`/mod/${id}/comment`);
+                if (comments.data.error) {
+                    console.log(comments.data.error);
+                    return;
+                }
+                commit("SET_COMMENTS", comments.data);
             } catch (e) {
                 console.log(e);
             }
@@ -68,6 +109,39 @@ export default createStore({
                     mod.instuctionHtml = renderMarkdown(mod.instruction);
                 }
                 state.mods.push(mod);
+            }
+        },
+        SET_MOD(state, mod) {
+            state.mod = mod;
+            if (mod.logo) {
+                state.mod.logo = getUploadUrl(mod.logo);
+            }
+        },
+        SET_AUTHOR(state, author) {
+            state.author = author;
+            if (author.avatar) {
+                state.author.avatar = getUploadUrl(state.author.avatar);
+            }
+        },
+        SET_SCREENSHOTS(state, screenshots) {
+            state.screenshots = [];
+            for (let screenshot of screenshots) {
+                screenshot.screenshot = getUploadUrl(screenshot.screenshot);
+                state.screenshots.push(screenshot);
+            }
+        },
+        SET_COMMENTS(state, comments) {
+            state.comments = [];
+            for (let comment of comments) {
+                if (comment.user.avatar) {
+                    comment.user.avatar = getUploadUrl(comment.user.avatar);
+                }
+                if (comment.user.registredAt) {
+                    comment.user.registredAt = relativeDate(comment.user.registredAt);
+                }
+                if (comment.comment.commentedAt) {
+                    comment.comment.commentedAt = relativeDate(comment.comment.commentedAt);
+                }
             }
         }
     }
