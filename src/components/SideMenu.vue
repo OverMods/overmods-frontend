1d58564c7c44f8cf3e29f8cd48fe587cde28212c
mykola2312 <49044616+mykola2312@users.noreply.github.com>
2023-06-27 17:18:36 +0300

    Update logo router-link

--- src/components/SideMenu.vue
+++ src/components/SideMenu.vue
@@ -1,12 +1,10 @@
 <template>
   <div class="side_menu">
     <div class="logo_box">
-      <router-link to="/">
-        <img src="../assets/images/icons/logo.png" alt="logo" class="logo">
-      </router-link>
-      <h3>OverMods</h3>
+      <router-link to="/"><img src="../assets/images/icons/logo.png" alt="logo" class="logo">
+        <h3>OverMods</h3></router-link>
       <hr class="underLogo_line">
-      <input class="search_box" id="gameSearch" placeholder="Search game" @input="onSearch">
+      <input class="search_box" id="gameSearch" placeholder="Search game">
     </div>
     <GameSelection ref="gameSelection"></GameSelection>
     <AuthBox></AuthBox>
