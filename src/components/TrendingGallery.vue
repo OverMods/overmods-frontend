88e674ad0b4344ba1097300f7b110f5789137586
mykola2312 <49044616+mykola2312@users.noreply.github.com>
2023-07-07 10:40:21 +0300

    Implement color auto-picking based on provided mod logo for trending gallery

--- src/components/TrendingGallery.vue
+++ src/components/TrendingGallery.vue
@@ -1,13 +1,14 @@
 <template>
   <div class="trending_gallery" @wheel="galleryScroll">
-    <div v-for="trend in trends" class="slide">
+    <div v-for="trend in trends" class="slide" :key="trend.mod.id">
       <router-link :to="`/game/${getGameById(trend.mod.game)?.shortName }/mod/${trend.mod.id}`">
         <h2>{{ getGameById(trend.mod.game)?.title }}</h2>
         <p>{{ trend.mod.title }}</p>
         <span>hover here</span>
         <div v-if="trend.mod.logo" over class="picture" :style="`background-image: url('${trend.mod.logo}')`"></div>
         <div v-else over class="picture"></div>
-        <button></button>
+        <img @load="onImageLoad($event, trend.mod.id)" :src="trend.mod.logo" alt="" style="display: none"/>
+        <button :style="`background-color: #${colors[trend.mod.id]};`"></button>
       </router-link>
     </div>
   </div>
@@ -18,12 +19,15 @@
 </style>
 
 <script setup>
-import { computed, onUpdated } from "vue";
+import { computed, ref, onUpdated } from "vue";
 import { useStore } from "vuex";
 import simpleSlider from "../simpleSlider.js";
 import { useRouter } from "vue-router";
+import ColorThief from "colorthief";
+
 const store = useStore();
 const router = useRouter();
+const colorThief = new ColorThief();
 
 const getGameById = computed(() => {
   return store.getters.getGameById;
@@ -32,17 +36,17 @@ const trends = computed(() => {
   return store.getters.getTrends;
 });
 
+const colors = ref({});
+function onImageLoad(e, id) {
+  const rgb = colorThief.getColor(e.target);
+  if (rgb) {
+    colors.value[id] = rgb[0].toString(16) + rgb[1].toString(16) + rgb[2].toString(16);
+    console.log(rgb[0].toString(16) + rgb[1].toString(16) + rgb[2].toString(16));
+  }
+}
+
 store.dispatch("fetchGameList");
 store.dispatch("fetchTrends");
-
-router.afterEach((to) => {
-  if (to.path === "/") {
-    window.addEventListener('load', () => {
-      simpleSlider();
-    });
-  }
-  return true;
-});
 
 onUpdated(() => {
   simpleSlider();
