26c8fdf3530dae328f5bbf056666c618b35b10f7
AmyresS <murlooo33@gmail.com>
2023-07-07 16:45:20 +0200

    merge

--- src/components/TrendingGallery.vue
+++ src/components/TrendingGallery.vue
@@ -23,12 +23,12 @@
 </style>
 
 <script setup>
-import { computed, ref, onUpdated } from "vue";
+import { computed, ref, onUpdated, watch } from "vue";
 import { useStore } from "vuex";
 import simpleSlider from "../simpleSlider.js";
-import { useRouter } from "vue-router";
+import ColorThief from "colorthief";
 const store = useStore();
-const router = useRouter();
+const colorThief = new ColorThief();
 
 const getGameById = computed(() => {
   return store.getters.getGameById;
@@ -39,6 +39,21 @@ const trends = computed(() => {
 
 store.dispatch("fetchGameList");
 store.dispatch("fetchTrends");
+
+const colors = ref({});
+function onImageLoad(e, id) {
+  const rgb = colorThief.getColor(e.target);
+  if (rgb) {
+    colors.value[id] = rgb[0].toString(16) + rgb[1].toString(16) + rgb[2].toString(16);
+    console.log(rgb[0].toString(16) + rgb[1].toString(16) + rgb[2].toString(16));
+  }
+}
+
+let trendsLength = 0;
+
+watch(trends, () => {
+  trendsLength = trends.value.length;
+});
 
 onUpdated(() => {
   if (trendsLength < 2){
