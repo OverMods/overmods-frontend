6056d8c8929aa9bfe35dbf80cee316f874dcc2f4
mykola2312 <49044616+mykola2312@users.noreply.github.com>
2024-05-15 05:56:11 +0300

    adjust http base urls to new infrastructure

--- src/http.js
+++ src/http.js
@@ -1,12 +1,13 @@
 import axios from "axios";
 
-export const API_PATH = "http://localhost/overmods";
-export const UPLOAD_DIR = "overmods-data";
+export const BASE_PATH = "http://192.168.100.4:31337";
+export const API_PATH = `${BASE_PATH}/api`
+export const UPLOAD_PATH = `${BASE_PATH}/data`;
 
 export const HTTP = axios.create({
    baseURL: API_PATH
 });
 
 export function getUploadUrl(filename) {
-   return `${API_PATH}/${UPLOAD_DIR}/${filename}`;
+   return `${UPLOAD_PATH}/${filename}`;
 }
